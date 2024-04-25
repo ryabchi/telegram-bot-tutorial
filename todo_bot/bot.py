d4d7b5c3e69417661c366c15e7f16acfee321a60
LeTim42 <tima142004@gmail.com>
2024-04-25 17:31:43 +0300

    Update todo bot

--- todo_bot/bot.py
+++ todo_bot/bot.py
@@ -22,6 +22,8 @@ from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandl
 
 from config import TELEGRAM_BOT_TOKEN
 
+TASKS = dict()
+
 # Enable logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -38,19 +40,33 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
     await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
+        rf"Привет, {user.mention_html()}!",
         reply_markup=ForceReply(selective=True),
     )
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /help is issued."""
-    await update.message.reply_text("Help!")
+    await update.message.reply_text("<тут должна была быть помощь>")
 
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def tasks_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /help is issued."""
+    user_id = update.effective_user.id
+    if user_id in TASKS:
+        await update.message.reply_text("Список задач:\n" + '\n'.join(TASKS[user_id]))
+    else:
+        await update.message.reply_text("У вас пока нет задач")
+
+
+async def text_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Echo the user message."""
-    await update.message.reply_text(update.message.text)
+    user_id = update.effective_user.id
+    task = update.message.text
+    if user_id not in TASKS:
+        TASKS[user_id] = []
+    TASKS[user_id].append(task)
+    await update.message.reply_text("Задача добавлена!")
 
 
 def main() -> None:
@@ -61,9 +77,10 @@ def main() -> None:
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(CommandHandler("tasks", tasks_command))
 
     # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, text_handler))
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
