70b38f8ce1535313ad8187e34793449f8cbaf29f
MateussssZ <mashimorin@edu.hse.ru>
2024-04-25 17:20:44 +0300

    Fix problem

--- todobot/bot.py
+++ todobot/bot.py
@@ -20,7 +20,7 @@ import logging
 from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
-from config import DefenceFromSkam
+from todobot.config import DefenceFromSkam
 
 # Enable logging
 logging.basicConfig(
